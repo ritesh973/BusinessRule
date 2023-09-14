2218ef2961cbd7c86f3ef934e76e9f686bfecde1
Ritesh Singh <rits@Riteshs-MacBook-Air.local>
2023-09-14 22:44:49 +0530

    Added initial Rules

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author ${USER}
+ * @created on ${DAY}/${MONTH}/${YEAR}
+ * @project ${PROJECT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
